402a458ba18a151564305e6a77163e50aeb58783
Bruvato <ray01778@gmail.com>
2025-03-02 10:46:44 -0500

    goof

--- app/auth/confirm/selectname/finishConfirmAction.ts
+++ app/auth/confirm/selectname/finishConfirmAction.ts
@@ -5,31 +5,34 @@ import { redirect } from "next/navigation";
 import { uniqueNamesGenerator, names } from "unique-names-generator";
 
 export async function selectUsername(formData: FormData) {
-    const supabase = await supabaseServer();
-    const name = formData.get("username") as string;
-    const id = (await supabase.auth.getUser()).data?.user?.id;
-    const {data, error} = await supabase.from("users").insert({
-        "id": id,
-        "username" : name
-    }).select();  
+  const supabase = await supabaseServer();
+  const name = formData.get("username") as string;
+  const id = (await supabase.auth.getUser()).data?.user?.id;
+  const { data, error } = await supabase
+    .from("users")
+    .insert({
+      id: id,
+      username: name,
+    })
+    .select();
 
-    console.log(data);
+  console.log(data);
 
-    if (error) {
-        console.log("ah shucks");
-        console.log((await supabase.auth.getUser()).data?.user?.role);
-        console.log(error);
-        redirect("/error");
-    }
+  if (error) {
+    console.log("ah shucks");
+    console.log((await supabase.auth.getUser()).data?.user?.role);
+    console.log(error);
+    redirect("/error");
+  }
 
-    for (let i = 0; i < 25; i++) {
-        const name = uniqueNamesGenerator({dictionaries: [names]});
-        const e2 = await supabase.from("transactions").insert({
-            "purchaser": (await supabase.auth.getUser()).data?.user?.id,
-            "seller": name,
-            "value": Math.trunc(Math.random() * 100000) / 100,
-        })
-    }
-    
-    redirect("/");
+  // for (let i = 0; i < 25; i++) {
+  //     const name = uniqueNamesGenerator({dictionaries: [names]});
+  //     const e2 = await supabase.from("transactions").insert({
+  //         "purchaser": (await supabase.auth.getUser()).data?.user?.id,
+  //         "seller": name,
+  //         "value": Math.trunc(Math.random() * 100000) / 100,
+  //     })
+  // }
+
+  redirect("/");
 }
